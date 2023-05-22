883d67fa41bf9f8e0686082a4945aaa44cd3d43d
sidgamer08 <78529308+sidgamer08@users.noreply.github.com>
2023-05-22 20:35:04 +0530

    Add files via upload

---
+++ import sys.py
@@ -0,0 +1,17 @@
+import sys
+import time
+import random
+import os
+import shutil
+from watchdog.observers import Observer
+from watchdog.events import
+FileSystemEventHandler
+from_dir="C:\Users\Sidhant\OneDrive\Desktop\Project102"
+on_created()
+on_modified()
+on_moved()
+on_deleted()
+# Initialize Event Handler class
+# Initialize observer
+# schedule the observer
+# start the observer
